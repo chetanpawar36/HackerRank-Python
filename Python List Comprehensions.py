2a9c4c536f244dc544e9c854db6c315cddbdc15f
chetanpawar36 <158987447+chetanpawar36@users.noreply.github.com>
2024-08-27 12:03:57 +0530

    Create Python List Comprehensions.py

---
+++ Python List Comprehensions.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    
+    x = int(input("enter the value: "))
+
+    y = int(input("enter the value: "))
+
+    z = int(input("enter the value: "))
+
+    n = int(input("enter the value: "))
+
+result = []
+
+for i in range(x+1):
+
+    for j in range(y+1):
+
+        for k in range(z+1):
+
+            if i+j+k != n:
+
+                result.append([i,j,k])
+
+print(result)
